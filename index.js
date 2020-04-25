cd67e3fc01317494203bef11e7471ddd6f20f30a
M Ahmed Mushtaq <mmushtaq.bese19seecs@seecs.edu.pk>
2020-04-26 04:45:32 +0500

    Emaily launch your new compaign

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express  =require("express");
+require("./services/passport");
+
+
 const app = express();
 
 
-app.get("/",async (req,res)=>{
-    res.send({hi:"there"});
-});
+require("./routes/authRoutes")(app);
 
 
 const PORT = process.env.PORT || 5000;
