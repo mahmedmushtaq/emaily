e1d593758eff3a69e469c06ebb0b2f5e7cb8ed8c
M Ahmed Mushtaq <mmushtaq.bese19seecs@seecs.edu.pk>
2020-04-29 07:09:46 +0500

    Added billing and client side

--- config/prod.js
+++ config/prod.js
@@ -3,4 +3,6 @@ module.exports  = {
     mongoURI:process.env.MONGO_URI,
     googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
     cookeyKey:process.env.COOKEY_KEY,
+    stripePublishableKey:process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
 }
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const keys = require('./config/keys');
 const mongooose = require("mongoose");
 const cookieSession  = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 require("./models/user");
 require("./services/passport");
 
@@ -19,9 +20,19 @@ app.use(cookieSession({
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.json());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'));
+    const path = require("path");
+    app.get('*',(req,res)=>{
+        res.sendFile(__dirname,"client","build","index.html");
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
