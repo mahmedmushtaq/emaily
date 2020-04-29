fad9c9f412c91c677b93eb61bf159a8b6c7f8fa5
M Ahmed Mushtaq <mmushtaq.bese19seecs@seecs.edu.pk>
2020-04-29 07:28:38 +0500

    Remove done error

--- index.js
+++ index.js
@@ -28,10 +28,12 @@ require("./routes/billingRoutes")(app);
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static('client/build'));
+
     const path = require("path");
-    app.get('*',(req,res)=>{
-        res.sendFile(__dirname,"client","build","index.html");
-    })
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+
 }
 
 const PORT = process.env.PORT || 5000;
